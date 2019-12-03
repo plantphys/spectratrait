f67aad0681eb345d35f43af06779419bca4c0557
Shawn P. Serbin <sserbin@bnl.gov>
2019-12-03 09:09:33 -0500

    Added example script R_Scripts/pull_data_from_ecosis.R

---
+++ R_Scripts/pull_data_from_ecosis.R
@@ -0,0 +1,162 @@
+####################################################################################################
+#
+#  
+#   A simple example showing how to pull spectra and trait data from EcoSIS and plot it up
+#
+#   Spectra and trait data source:
+#   https://ecosis.org/package/ngee-arctic-2016-leaf-spectral-reflectance-kougarok-road-seward-peninsula-alaska-2016
+#
+#    Notes:
+#    * Provided as a basic example of how to apply the model to new spectra observations
+#    * The author notes the code is not the most elegant or clean, but is functional 
+#    * Questions, comments, or concerns can be sent to sserbin@bnl.gov
+#    * Code is provided under GNU General Public License v3.0 
+#
+#
+#    --- Last updated:  12.03.2019 By Shawn P. Serbin <sserbin@bnl.gov>
+####################################################################################################
+
+
+#---------------- Close all devices and delete all variables. -------------------------------------#
+rm(list=ls(all=TRUE))   # clear workspace
+graphics.off()          # close any open graphics
+closeAllConnections()   # close any open connections to files
+
+list.of.packages <- c("devtools","readr","scales","plotrix","httr","pls","dplyr",
+                      "reshape2","ggplot2")  # packages needed for script
+# check for dependencies and install if needed
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# load libraries needed for script
+library(readr)    # readr - read_csv function to pull data from EcoSIS
+library(plotrix)  # plotCI - to generate obsvered vs predicted plot with CIs
+library(scales)   # alpha() - for applying a transparency to data points
+library(devtools)
+library(pls)
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+
+# define function to grab PLSR model from GitHub
+#devtools::source_gist("gist.github.com/christophergandrud/4466237")
+source_GitHubData <-function(url, sep = ",", header = TRUE) {
+  require(httr)
+  request <- GET(url)
+  stop_for_status(request)
+  handle <- textConnection(content(request, as = 'text'))
+  on.exit(close(handle))
+  read.table(handle, sep = sep, header = header)
+}
+
+# not in
+`%notin%` <- Negate(`%in%`)
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Set working directory (scratch space)
+output_dir <- file.path("~",'scratch/')
+if (! file.exists(output_dir)) dir.create(output_dir,recursive=TRUE, showWarnings = FALSE)
+setwd(output_dir) # set working directory
+getwd()  # check wd
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Example datasets
+# 
+# URL:  https://ecosis.org/package/ngee-arctic-2016-leaf-spectral-reflectance-kougarok-road-seward-peninsula-alaska-2016
+#
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Grab data
+print("**** Downloading Ecosis data ****")
+ecosis_id <- "960dbb0c-144e-4563-8117-9e23d14f4aa9"  # NGEE-Arctic dataset
+ecosis_file <- sprintf(
+  "https://ecosis.org/api/package/%s/export?metadata=true",
+  ecosis_id
+)
+message("Downloading data...")
+dat_raw <- read_csv(ecosis_file)
+message("Download complete!")
+names(dat_raw)[1:40]
+head(dat_raw)
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Prepare data
+Start.wave <- 500
+End.wave <- 2400
+wv <- seq(Start.wave,End.wave,1)
+spectra <- data.frame(dat_raw[,names(dat_raw) %in% wv])
+names(spectra) <- c(paste0("Wave_",wv))
+head(spectra)[,1:5]
+
+sample_info <- dat_raw[,names(dat_raw) %notin% seq(350,2500,1)]
+head(sample_info)
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Plot spectra
+cexaxis <- 1.5
+cexlab <- 1.8
+ylim <- 65
+ylim2 <- 65
+
+mean_spec <- colMeans(spectra[,which(names(spectra) %in% paste0("Wave_",wv))])
+spectra_quantiles <- apply(spectra[,which(names(spectra) %in% paste0("Wave_",wv))],
+                           2,quantile,na.rm=T,probs=c(0,0.025,0.05,0.5,0.95,0.975,1))
+
+print("**** Plotting Ecosis data. Writing to scratch space ****")
+png(file=file.path(output_dir,'NGEE-Arctic_2016_Kougarok_leaf_spectra_summary_plot.png'),height=3000,
+    width=3900, res=340)
+par(mfrow=c(1,1), mar=c(4.5,5.7,0.3,0.4), oma=c(0.3,0.9,0.3,0.1)) # B, L, T, R
+plot(wv,mean_spec,ylim=c(0,ylim),cex=0.00001, col="white",xlab="Wavelength (nm)",
+     ylab="Reflectance (%)",cex.axis=cexaxis, cex.lab=cexlab)
+polygon(c(wv ,rev(wv)),c(spectra_quantiles[6,], rev(spectra_quantiles[2,])),
+        col="#99CC99",border=NA)
+lines(wv,mean_spec,lwd=3, lty=1, col="black")
+lines(wv,spectra_quantiles[1,]*100,lwd=1.85, lty=3, col="grey40")
+lines(wv,spectra_quantiles[7,]*100,lwd=1.85, lty=3, col="grey40")
+legend("topright",legend=c("Mean reflectance","Min/Max", "95% CI"),lty=c(1,3,1),
+       lwd=c(3,3,15),col=c("black","grey40","#99CC99"),bty="n", cex=1.7)
+box(lwd=2.2)
+dev.off()
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Plot trait data
+names(sample_info)
+trait_data <- sample_info %>%
+  select(Site,Sample_ID,USDA_Species_Code=`USDA Symbol`,Common_Name=`Common Name`,LMA_gDW_m2=LMA_g_m2,
+         Nmass_g_g,N_area_g_m2,Cmass_g_g,C_area_g_m2,CN_Ratio)
+head(trait_data)
+
+trait_data2 <- melt(data = trait_data, id.vars = "USDA_Species_Code", measure.vars = c("LMA_gDW_m2",
+                                                                                       "Nmass_g_g",
+                                                                                       "N_area_g_m2",
+                                                                                       "CN_Ratio"))
+head(trait_data2)
+
+
+ggplot(trait_data2, aes(x=USDA_Species_Code, y=note, fill=treatment)) + 
+  geom_boxplot()
+
+
+p2 <- ggplot(trait_data2, aes(x=USDA_Species_Code, y=value)) + 
+  geom_boxplot() +
+  facet_wrap(~variable, scale="free")
+ggsave(filename = file.path(output_dir,"NGEE-Arctic_2016_Kougarok_Trait_data.png"), plot = p2,
+       width = 40, height = 20, units = "cm")
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+rm(list=ls(all=TRUE))   # clear workspace
+### EOF
