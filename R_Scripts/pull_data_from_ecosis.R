fc956ecc570fc718caa11954a20920931dbb1d18
Shawn P. Serbin <sserbin@bnl.gov>
2020-05-11 09:16:13 -0400

    Updated R_Scripts/pull_data_from_ecosis.R. Added pull_data_from_ecosis_example.Rmd to vignettes

--- R_Scripts/pull_data_from_ecosis.R
+++ R_Scripts/pull_data_from_ecosis.R
@@ -13,7 +13,7 @@
 #    * Code is provided under GNU General Public License v3.0 
 #
 #
-#    --- Last updated:  12.03.2019 By Shawn P. Serbin <sserbin@bnl.gov>
+#    --- Last updated:  05.11.2020 By Shawn P. Serbin <sserbin@bnl.gov>
 ####################################################################################################
 
 
@@ -22,18 +22,13 @@ rm(list=ls(all=TRUE))   # clear workspace
 graphics.off()          # close any open graphics
 closeAllConnections()   # close any open connections to files
 
-list.of.packages <- c("devtools","readr","scales","plotrix","httr","pls","dplyr",
-                      "reshape2","ggplot2")  # packages needed for script
+list.of.packages <- c("readr","httr","dplyr","reshape2","ggplot2")  # packages needed for script
 # check for dependencies and install if needed
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 # load libraries needed for script
 library(readr)    # readr - read_csv function to pull data from EcoSIS
-library(plotrix)  # plotCI - to generate obsvered vs predicted plot with CIs
-library(scales)   # alpha() - for applying a transparency to data points
-library(devtools)
-library(pls)
 library(dplyr)
 library(reshape2)
 library(ggplot2)
@@ -112,7 +107,7 @@ mean_spec <- colMeans(spectra[,which(names(spectra) %in% paste0("Wave_",wv))])
 spectra_quantiles <- apply(spectra[,which(names(spectra) %in% paste0("Wave_",wv))],
                            2,quantile,na.rm=T,probs=c(0,0.025,0.05,0.5,0.95,0.975,1))
 
-print("**** Plotting Ecosis data. Writing to scratch space ****")
+print("**** Plotting Ecosis specrtal data. Writing to scratch space ****")
 png(file=file.path(output_dir,'NGEE-Arctic_2016_Kougarok_leaf_spectra_summary_plot.png'),height=3000,
     width=3900, res=340)
 par(mfrow=c(1,1), mar=c(4.5,5.7,0.3,0.4), oma=c(0.3,0.9,0.3,0.1)) # B, L, T, R
@@ -132,24 +127,24 @@ dev.off()
 
 #--------------------------------------------------------------------------------------------------#
 ### Plot trait data
+print("**** Plotting Ecosis trait data. Writing to scratch space ****")
+
+# Organize leaf trait data
 names(sample_info)
 trait_data <- sample_info %>%
   select(Site,Sample_ID,USDA_Species_Code=`USDA Symbol`,Common_Name=`Common Name`,LMA_gDW_m2=LMA_g_m2,
          Nmass_g_g,N_area_g_m2,Cmass_g_g,C_area_g_m2,CN_Ratio)
 head(trait_data)
 
-trait_data2 <- melt(data = trait_data, id.vars = "USDA_Species_Code", measure.vars = c("LMA_gDW_m2",
-                                                                                       "Nmass_g_g",
-                                                                                       "N_area_g_m2",
-                                                                                       "CN_Ratio"))
-head(trait_data2)
+# Prepare data for ggplot
+trait_data <- melt(data = trait_data, id.vars = "USDA_Species_Code", measure.vars = c("LMA_gDW_m2",
+                                                                                      "Nmass_g_g",
+                                                                                      "N_area_g_m2",
+                                                                                      "CN_Ratio"))
+head(trait_data)
 
-
-ggplot(trait_data2, aes(x=USDA_Species_Code, y=note, fill=treatment)) + 
-  geom_boxplot()
-
-
-p2 <- ggplot(trait_data2, aes(x=USDA_Species_Code, y=value)) + 
+# Graph the trait data and save a file to the scratch space
+p2 <- ggplot(trait_data, aes(x=USDA_Species_Code, y=value)) + 
   geom_boxplot() +
   facet_wrap(~variable, scale="free")
 ggsave(filename = file.path(output_dir,"NGEE-Arctic_2016_Kougarok_Trait_data.png"), plot = p2,
