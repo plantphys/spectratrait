a5071eb5c844bcbdaed4ad3aa07805cc680e3e07
Shawn P. Serbin <sserbin@bnl.gov>
2020-09-15 17:05:34 -0400

    Updated vignette.  Bugs found in data split function

---
+++ R_Scripts/expanded_spectra-trait_kit_lma_plsr_example.R
@@ -0,0 +1,129 @@
+####################################################################################################
+#
+#  
+#   An expanded example "How-to" script illustrating the use of PLSR modeling to develop a 
+#   spectra-trait algorithm to estimate leaf mass area with leaf-level spectroscopy data. The 
+#   example is built from published data source from the EcoSIS spectral database. This examples
+#   illustrates an approach to quantify model prediction uncertainty based on a jackknife analysis
+#
+#   Spectra and trait data source:
+#   https://ecosis.org/package/leaf-reflectance-plant-functional-gradient-ifgg-kit
+#
+#    Notes:
+#    * The author notes the code is not the most elegant or clean, but is functional 
+#    * Questions, comments, or concerns can be sent to sserbin@bnl.gov
+#    * Code is provided under GNU General Public License v3.0 
+#
+####################################################################################################
+
+rm(list=ls(all=TRUE))   # clear workspace
+graphics.off()          # close any open graphics
+closeAllConnections()   # close any open connections to files
+
+#--------------------------------------------------------------------------------------------------#
+### Install and load required R packages
+list.of.packages <- c("devtools","readr","RCurl","httr","pls","dplyr","reshape2","here",
+                      "ggplot2","gridExtra")  # packages needed for script
+# check for dependencies and install if needed
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# Load libraries
+invisible(lapply(list.of.packages, library, character.only = TRUE))
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Setup other functions and options
+github_dir <- file.path(here(),"R_Scripts")
+source_from_gh <- TRUE
+if (source_from_gh) {
+  # Source helper functions from GitHub
+  devtools::source_url("https://raw.githubusercontent.com/TESTgroup-BNL/PLSR_for_plant_trait_prediction/master/R_Scripts/functions.R")
+} else {
+  functions <- file.path(github_dir,"functions.R")
+  source(functions)
+}
+
+# not in
+`%notin%` <- Negate(`%in%`)
+
+# Script options
+pls.options(plsralg = "oscorespls")
+pls.options("plsralg")
+
+# Default par options
+opar <- par(no.readonly = T)
+
+# What is the target variable?
+inVar <- "LMA_gDW_m2"
+
+# What is the source dataset from EcoSIS?
+ecosis_id <- "3cf6b27e-d80e-4bc7-b214-c95506e46daa"
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Set working directory (scratch space)
+outdir <- tempdir()
+setwd(outdir) # set working directory
+getwd()  # check wd
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Get source dataset from EcoSIS
+dat_raw <- get_ecosis_data(ecosis_id = ecosis_id)
+head(dat_raw)
+names(dat_raw)[1:40]
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Create plsr dataset
+Start.wave <- 500
+End.wave <- 2400
+wv <- seq(Start.wave,End.wave,1)
+Spectra <- as.matrix(dat_raw[,names(dat_raw) %in% wv])
+colnames(Spectra) <- c(paste0("Wave_",wv))
+head(Spectra)[1:6,1:10]
+sample_info <- dat_raw[,names(dat_raw) %notin% seq(350,2500,1)]
+head(sample_info)
+
+sample_info2 <- sample_info %>%
+  select(Plant_Species=species,Growth_Form=`growth form`,timestamp,SLA_g_cm=`SLA (g/cm )`)
+head(sample_info2)
+
+plsr_data <- data.frame(sample_info2,Spectra)
+rm(sample_info,sample_info2,Spectra)
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Create cal/val datasets
+## Make a stratified random sampling in the strata USDA_Species_Code and Domain
+
+method <- "base" #base/dplyr
+# base R - a bit slow
+# dplyr - much faster
+split_data <- create_data_split(approach=method, split_seed=2356812, prop=0.8, 
+                                group_variables=c("Plant_Species"))
+names(split_data)
+cal.plsr.data <- split_data$cal_data
+head(cal.plsr.data)[1:8]
+val.plsr.data <- split_data$val_data
+head(val.plsr.data)[1:8]
+rm(split_data)
+
+# Datasets:
+print(paste("Cal observations: ",dim(cal.plsr.data)[1],sep=""))
+print(paste("Val observations: ",dim(val.plsr.data)[1],sep=""))
+
+cal_hist_plot <- qplot(cal.plsr.data[,paste0(inVar)],geom="histogram",
+                       main = paste0("Cal. Histogram for ",inVar),
+                       xlab = paste0(inVar),ylab = "Count",fill=I("grey50"),col=I("black"),alpha=I(.7))
+val_hist_plot <- qplot(val.plsr.data[,paste0(inVar)],geom="histogram",
+                       main = paste0("Val. Histogram for ",inVar),
+                       xlab = paste0(inVar),ylab = "Count",fill=I("grey50"),col=I("black"),alpha=I(.7))
+grid.arrange(cal_hist_plot, val_hist_plot, ncol=2)
+#--------------------------------------------------------------------------------------------------#
