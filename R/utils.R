57c82ef2b0df27aa562e08cc59e6ceb7a90d964d
Shawn P. Serbin <sserbin@bnl.gov>
2021-03-10 11:47:51 -0500

    Fixes addressing #78. A WIP

--- R/utils.R
+++ R/utils.R
@@ -18,6 +18,39 @@ source_GitHubData <- function(url, sep = ",", header = TRUE) {
   read.table(handle, sep = sep, header = header)
 }
 
+##' Calculate RMSE and percent RMSE with PLSR model results
+##' 
+##' @param plsr_dataset input plsr dataset
+##' @param inVar the trait variable used in the calculation of RMSE
+##' @param residuals predicted minus observed residual vector from either 
+##' a cross-validation CV or independent validation
+##' @param range calculate over the full data range or the 95% of data range. 
+##' options full or 95perc
+##' 
+##' @author Shawn P. Serbin
+##' 
+##' @export
+percent_rmse <- function(plsr_dataset = NULL, inVar = NULL, 
+                         residuals = NULL, range = "full") {
+  rmse <- sqrt(mean(residuals^2, na.rm = TRUE))
+  val_data_range <- range(plsr_dataset[,inVar], na.rm = TRUE)
+  val_95perc_data_range <- quantile(plsr_dataset[,inVar], 
+                                    probs = c(0.025,0.975), 
+                                    na.rm = TRUE)
+  if (range=="full") {
+    perc_rmse <- (rmse/(val_data_range[2]-val_data_range[1]))*100
+  } else if (range=="95perc") {
+    perc_rmse <- (rmse/(val_95perc_data_range[[2]]-val_95perc_data_range[[1]]))*100 
+  } else {
+    print("Not a valid option, defaulting to full")
+    perc_rmse <- (rmse/(val_data_range[2]-val_data_range[1]))*100
+  }
+  
+  # output rmse list
+  output <- list(rmse = rmse, perc_rmse = perc_rmse)
+  return(output)
+}
+
 ##' Function to check for installed package
 ##' not presently used
 testForPackage <- function(pkg) {
